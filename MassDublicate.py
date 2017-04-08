c50250eb1fb16e068fd93ad36aed2a2caecaf48c
Kutyavin <vityakut@gmail.com>
2017-04-08 21:41:45 +0300

    init

---
+++ MassDublicate.py
@@ -0,0 +1,33 @@
+import sublime
+import sublime_plugin
+
+
+class MassDublicateCommand(sublime_plugin.TextCommand):
+	def run_(self, edit_token, args):
+		args = self.filter_args(args)
+		if args:
+			edit = self.view.begin_edit(edit_token, self.name())
+			pos = self.view.sel()[0].end()
+			if not self.view.sel()[0].empty():
+				input_str = '\n' + self.view.substr(self.view.sel()[0])
+				# self.view.insert(edit, pos, input_str*int(args))
+				self.view.insert(edit, pos, input_str*int(args))
+			else:
+				sublime.status_message("Selection empty")
+			self.view.end_edit(edit)
+
+
+
+
+
+class GetNumDublicateCommand(sublime_plugin.WindowCommand):
+	def run(self):
+		self.window.show_input_panel("Количество дубликатов:", "", self.on_done, None, None)
+
+
+	def on_done(self, num):
+
+		if num.isdigit():
+			self.window.run_command("mass_dublicate", num)
+		else:
+			sublime.status_message("Error: " + num + " is not number")
